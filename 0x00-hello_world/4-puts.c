e70439071485baf39ddb38d093bfc7c160e82aad
Indifestus <indifestus@gmail.com>
2022-06-30 16:34:15 +0000

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multiligual puzzle");
+	return (0);
+}
+
