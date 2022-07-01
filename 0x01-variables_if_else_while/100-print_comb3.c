205a15274319622af76e15b119ce051ca4659e32
Festus <107360958+Indifestus@users.noreply.github.com>
2022-07-02 02:56:00 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,36 @@
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 #include <stdio.h>
-
+/**
+ * main - main function
+ *
+ * Return: end program
+ */
 int main(void)
 {
-int i, j;
+	int a = 0;
+	int b = 0;
+	int coma = 1;
 
-for (j = 48 ; j < 58 ; j++)
-{
-for (i = 48 ; i < 58 ; i++)
-{
-putchar(j);
-putchar(i);
-if (j != 57 || i != 57)
-{
-putchar(44);
-putchar(32);
-}
-}
-}
-putchar('\n');
-return (0);
+	while (a <= 9)
+	{
+		while (b <= 9)
+		{
+			if (a < b)
+			{
+				if (coma == 0)
+				{
+					putchar(',');
+					putchar(32);
+				}
+				coma = 0;
+				putchar(a + '0');
+				putchar(b + '0');
+			}
+			++b;
+		}
+		++a;
+		b = 0;
+	}
+	putchar(10);
+
+	return (0);
 }
