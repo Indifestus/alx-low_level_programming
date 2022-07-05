22f175c791878b3359934c19319a997482edf988
Indifestus <indifestus@gmail.com>
2022-07-05 20:03:19 +0000

    0_putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include<unisd.h>
+/**
+ * main -main fuction
+ *
+ * Return: End program
+ */
+int main(void)
+{
+	char string[8] = "_putchar";
+	int n = 0;
+
+	while (n < 8)
+
+	{
+		_putchar(string[n]);
+		++n;
+
+	}
+
+	_putchar(10);
+
+	return (0);
+
+}
