a2fb76195c5d933e61d3f141c69b22479fccd4af
Indifestus <indifestus@gmail.com>
2022-07-07 06:11:48 -0700

    Leap year

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -14,5 +15,5 @@ int largest_number(int a, int b, int c)
 
 	largest = (a > b) ? a : b;
 	largest = (largest > c) ? largest : c;
-	return (largest);
+	printf (largest);
 }
