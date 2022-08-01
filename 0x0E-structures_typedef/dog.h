41a936904c7f7aacee1d3877efd70d67311dfcf7
Indifestus <indifestus@gmail.com>
2022-08-01 12:25:11 -0700

    changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,27 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 /**
+ * struct dog - Create the structure dog
+ *
  * @name: First member
- * @ge: Second member
+ * @age: Second member
  * @owner: Third member
  *
- * Decripion: Create the structure dog
+ * Description: Create the structure dog
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-typedef struct dog dog_t;
 
 #endif
