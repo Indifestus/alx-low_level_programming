af21f6d8f7682a165c16839060d09a0bae4e2281
Indifestus <indifestus@gmail.com>
2022-08-01 11:43:10 -0700

    First commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ *
+ * @name: First member
+ * @age: Second member 
+ * @owner: Third member
+ *
+ * Decripion: Create the structure dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+typedef struct dog dog_t;
+
+#endif
